e49b0e0c8edd20ba4824b862b80bace9b7fbe891
budyhartono55 <budyhartono55@gmail.com>
2023-04-02 21:31:23 +0800

    add client interface

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2"); //import mysql
 
 // Create the connection pool. The pool-specific settings are the defaults
 const db_pool = mysql.createPool({
-  host: sql12.freemysqlhosting.net,
-  user: sql12610282,
-  database: sql12610282,
-  password: Rb8NAjHYhQ,
+  host: "sql12.freemysqlhosting.net",
+  user: "sql12610282",
+  database: "sql12610282",
+  password: "Rb8NAjHYhQ",
 });
 
 module.exports = db_pool.promise();
